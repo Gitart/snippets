c56868e3d20a49663e0f9bd84b1b48f674f18e49
Arthur Savage <alerc@meta.ua>
2019-08-17 18:28:38 +0300

    Create session.go

---
+++ session.go
@@ -0,0 +1,12 @@
+
+// Session
+func session(w http.ResponseWriter, r *http.Request)(sess data.Session, err error){  
+cookie, err := r.Cookie("_cookie")  
+ if err == nil {sess = data.Session{Uuid: cookie.Value}    
+   if ok, _ := sess.Check(); !ok {err = errors.New("Invalid session")}  
+  }    
+return
+}
+
+The session function retrieves the cookie from the request: 
+cookie, err := r.Cookie("_cookie")Listing 2.5session utility function in util.go
